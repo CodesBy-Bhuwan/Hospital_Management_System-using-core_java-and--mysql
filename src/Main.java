4b63548fb56f13d1f7949d2c49fc8d02c32cd932
Codesby-Bhuwan <b.coding817@gamil.com>
2024-01-05 00:03:03 +0545

    Successfully connected to jdbc db

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+//        String url = "http://localhost:3306/hospital_data";
+        String url = "jdbc:mysql://localhost:3306/hospital_data";
+
+        String username = "root";
+        String password = "";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)){
+        System.out.println("connected to database");
+//        This technique below is used to check where the connection is made.
+            System.out.println(connection);
+    } catch (SQLException e){
+        System.err.println("connection failed" + e.getMessage());
     }
+    }
+
 }
+
